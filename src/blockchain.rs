998f357f577aeeef801a5744312a73beec86a486
Yakov Annikov <annikoff@ya.ru>
2018-07-19 17:33:46 +0300

    Fix Blockchain#block. Add Blockchain#create_block. Remove Blockchain#push

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,5 +1,6 @@
 use block::Block;
 
+#[derive(Debug)]
 pub struct Blockchain {
     blocks: Vec<Block>
 }
@@ -23,7 +24,7 @@ impl Blockchain {
     }
 
     pub fn block(&self, index: usize) -> Option<&Block> {
-        if self.blocks.len() > 0 && self.blocks.len() < index {
+        if self.blocks.len() > 0 && index < self.blocks.len() {
             Some(&self.blocks[index])
         } else {
             None
@@ -37,12 +38,16 @@ impl Blockchain {
         }
     }
 
-    pub fn push(&mut self, block: Block) -> () {
-        self.blocks.push(block)
-    }
-
     pub fn len(&self) -> usize {
         self.blocks.len()
+    }
+
+    pub fn create_block(&mut self, content: &String) -> () {
+        let last_block = self.last_block().cloned().unwrap();
+        let previous_hash = last_block.hash().to_string();
+
+        let block = Block::new(last_block.index() + 1, &previous_hash, &content);
+        self.blocks.push(block)
     }
 }
 
@@ -54,32 +59,31 @@ mod tests {
     fn create_blockchain_with_genesis_block() {
         let blockchain = Blockchain::new();
         let block = blockchain.block(0).unwrap();
+
         assert_eq!(block.content(), "Genesis block");
         assert_eq!(block.previous_hash(),
                    "0000000000000000000000000000000000000000000000000000000000000000");
     }
 
     #[test]
-    fn get_last_block() {
+    fn last_block() {
         let blockchain = Blockchain::new();
         assert_eq!(blockchain.blocks().len(), 1);
         assert_eq!(blockchain.last_block().unwrap().index(), 0);
     }
 
     #[test]
-    fn push_new_block_into_blockchain() {
+    fn create_block() {
         let mut blockchain = Blockchain::new();
-        let last_block = blockchain.last_block().cloned().unwrap();
+        let previous_block = blockchain.last_block().cloned().unwrap();
         let content = String::from("Next block");
-        let previous_hash = last_block.hash().to_string();
 
-        let block = Block::new(last_block.index() + 1, &previous_hash, &content);
-        blockchain.push(block);
+        blockchain.create_block(&content);
 
-        let pushed_block = blockchain.last_block().cloned().unwrap();
+        let next_block = blockchain.last_block().cloned().unwrap();
         assert_eq!(blockchain.len(), 2);
-        assert_eq!(pushed_block.previous_hash(), last_block.hash());
-        assert_eq!(pushed_block.index(), last_block.index() + 1);
-        assert_eq!(pushed_block.content(), content);
+        assert_eq!(next_block.previous_hash(), previous_block.hash());
+        assert_eq!(next_block.index(), previous_block.index() + 1);
+        assert_eq!(next_block.content(), content);
     }
 }
